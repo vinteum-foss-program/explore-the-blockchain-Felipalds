e3ca99fd2ad35fdc5144ee9aa4678204f128d58f
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-11-23 14:15:12 -0300

    4 again

--- submission/004.sh
+++ submission/004.sh
@@ -8,4 +8,4 @@ pk=xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr
 # echo "$pk"
 checksum=$(bitcoin-cli getdescriptorinfo "tr($pk/0/*)" | jq -r '.checksum')
 # echo $checksum
-bitcoin-cli deriveaddresses "tr($pk/0/*)#$checksum" "[100,101]" | jq -r '.[0]'
+bitcoin-cli deriveaddresses "tr($pk/0/*)#$checksum" "[100,101]" | jq -r '.[1]'

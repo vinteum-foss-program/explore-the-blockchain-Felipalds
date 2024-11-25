c5f61a02e52c9766a85f91b446422cc581672ccc
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-11-24 21:09:45 -0300

    8

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+
+bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1 | jq -r '.vin[0].txinwitness[2]' | cut -c 5-70
